a0b1b313334a583ce268ac1fa8320fec71cb978a
WandrilleD <wandrille.duchemin@unibas.ch>
2026-09-18 08:48:33 +0200

    chapter4 simpler exercise

--- notebooks/solutions/solution_boruta.py
+++ notebooks/solutions/solution_boruta.py
@@ -1,4 +1,4 @@
-## 2. use cross-validation to estimate the accuracy of a random forest on this dataset
+## 0. use cross-validation to estimate the accuracy of a random forest on this dataset
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
@@ -19,16 +19,16 @@ RF_fi = rf_grid.best_estimator_.feature_importances_
 print( f"{( RF_fi != 0  ).sum()}/{X.shape[1]} features with non-zero importances")
 
 #######
-## 3. use boruta to select all-relevant variables
+## 1. use boruta to select all-relevant variables
 from boruta import BorutaPy
 
-rf = RandomForestClassifier(n_jobs=-1, ccp_alpha = rf_grid.best_params_['ccp_alpha'])
+rf = RandomForestClassifier(n_jobs=-1, ccp_alpha = 1.438449888287663e-06)
 feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=123, max_iter = 250 )
 
 %time feat_selector.fit(X, y)
 
 #######
-## 4. keep only the features selected by boruta and use cross-validation to estimate the accuracy of a random forest 
+## 2. keep only the features selected by boruta and use cross-validation to estimate the accuracy of a random forest 
 
 ## keep only the confirmed
 Xf = feat_selector.transform( np.array(X) )
@@ -40,11 +40,11 @@ Xf = feat_selector.transform( np.array(X) , weak = True )
 print("cross-validated score with confirmed+tentative features" ,cross_val_score(rf, Xf,y).mean() )
 
 #######
-## 5. run Boruta again, but with the `perc` parameter to 80 instead of 100. How many more variables does it select?
+## 3. run Boruta again, but with the `perc` parameter to 80 instead of 100. How many more variables does it select?
 
 from boruta import BorutaPy
 
-rf = RandomForestClassifier(n_jobs=-1, ccp_alpha = rf_grid.best_params_['ccp_alpha'])
+rf = RandomForestClassifier(n_jobs=-1, ccp_alpha = 1.438449888287663e-06)
 feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=123, max_iter = 250 , perc=80)
 
 %time feat_selector.fit(X, y)
